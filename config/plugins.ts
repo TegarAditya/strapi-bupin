c8f7edb5ecb10c7c8bc6d824d6b016642dcd31a1
Tegar Aditya <tegaraditya53@gmail.com>
2024-11-03 06:48:54 +0700

    Add server configurations to documentation and update API URLs

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+export default () => ({
+    documentation: {
+        config: {
+            servers: [
+                { url: 'http://localhost:1337/api', description: 'Development server' },
+                { url: 'https://content.bupin.id/api', description: 'Production server' },],
+        }
+    }
+});
